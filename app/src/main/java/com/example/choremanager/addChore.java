58cf9b55efa3e0ee35a246def5ed29063efdd401
Aidan Toscano <aidantoscano03@gmail.com>
2020-12-19 07:53:34 -0800

    As John said, "We Pushin'"

--- app/src/main/java/com/example/choremanager/addChore.java
+++ app/src/main/java/com/example/choremanager/addChore.java
@@ -53,6 +53,7 @@ public class addChore extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
