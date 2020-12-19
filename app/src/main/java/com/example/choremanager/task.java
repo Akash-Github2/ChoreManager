671f6704b7034ff5f1e48e043f589464b1d57c2c
John Carraher <jkcarraher@gmail.com>
2020-12-19 09:13:43 -0800

    we pushin more plus spelling, reseting Oncreate, context

---
+++ app/src/main/java/com/example/choremanager/task.java
@@ -0,0 +1,13 @@
+package com.example.choremanager;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class task {
+
+    Date dueTimeDate = Calendar.getInstance().getTime();
+    Date createdTimeDate = Calendar.getInstance().getTime();
+
+
+
+}
