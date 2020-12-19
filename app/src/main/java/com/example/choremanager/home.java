b33109950fc4c133db2b833bee431b310ef9546a
John Carraher <jkcarraher@gmail.com>
2020-12-19 01:13:57 -0800

    we pushin more plus spelling, reseting Oncreate, context

--- app/src/main/java/com/example/choremanager/home.java
+++ app/src/main/java/com/example/choremanager/home.java
@@ -13,63 +13,21 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class home extends Fragment {
 
-    ListView listview ;
-    String[] ListViewItems = new String[] {
-            "ListView ITEM-1",
-            "ListView ITEM-2",
-            "ListView ITEM-3",
-            "ListView ITEM-4",
-            "ListView ITEM-5",
-            "ListView ITEM-6",
-            "ListView ITEM-7",
-            "ListView ITEM-8",
-            "ListView ITEM-9",
-            "ListView ITEM-10"
-
-    };
-
-    SparseBooleanArray sparseBooleanArray ;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listview ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        listview = view.findViewById(R.id.listView);
+        listview =  view.findViewById(R.id.listView);
+        itemsAdapter.add("Do DA DISHES");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_multiple_choice, android.R.id.text1, ListViewItems );
-
-        listview.setAdapter(adapter);
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // TODO Auto-generated method stub
-
-                sparseBooleanArray = listview.getCheckedItemPositions();
-
-                String ValueHolder = "" ;
-
-                int i = 0 ;
-
-                while (i < sparseBooleanArray.size()) {
-
-                    if (sparseBooleanArray.valueAt(i)) {
-
-                        ValueHolder += ListViewItems [ sparseBooleanArray.keyAt(i) ] + ",";
-                    }
-
-                    i++ ;
-                }
-
-                ValueHolder = ValueHolder.replaceAll("(,)*$", "");
-
-                //Toast.makeText(MainActivity.this, "ListView Selected Values = " + ValueHolder, Toast.LENGTH_LONG).show();
-
-            }
-        });
         return view;
     }
 }
